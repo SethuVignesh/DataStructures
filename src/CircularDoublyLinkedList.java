44b81174caf07142720177ab28abd3f6b6fa0523
Sethu <sethu.store@gmail.com>
2017-09-18 14:28:13 +0530

    feat: created method signatures for csll cdll

---
+++ src/CircularDoublyLinkedList.java
@@ -0,0 +1,15 @@
+public class CircularDoublyLinkedList {
+    int size;
+    DNode next;
+    DNode prev;
+    DNode head;
+
+    public void append(int data){}
+    public void isEmpty(){}
+    public void insertAtBegin(int data){}
+    public void insertAtPos(int data, int pos){}
+    public void reverse(){}
+    public void insertAsc(){}
+    public void getMiddle(){}
+    public void delete(){}
+}
